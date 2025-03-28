90f32867a134c7ad612be3465bc559cc9ab60b40
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-03-28 13:53:07 -0400

    Initial commit

---
+++ stern_hackathon_demo/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package Description.
+"""
+
+from ._version import __version__
+
+__short_description__ = "Package short description."
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "sanhehu@easyscalecloud.com"
+__maintainer__ = "Sanhe Hu"
+__maintainer_email__ = "sanhehu@easyscalecloud.com"
+__github_username__ = "easyscalecloud"
