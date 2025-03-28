1252d0065452ea542f78334f9db97653710764b1
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-03-28 14:56:19 -0400

    chore

--- stern_hackathon_demo/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .helper import run_unit_test, run_cov_test
--- tests/all.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-if __name__ == "__main__":
-    from stern_hackathon_demo.tests import run_cov_test
-
-    run_cov_test(__file__, "stern_hackathon_demo", is_folder=True, preview=False)
